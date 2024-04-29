8ef1f21c07d9d0cd3ec90c5dbd4a24906203381b
Tim Hodges <tim.hodges@smartsheet.com>
2024-04-29 15:06:47 -0700

    fixing up contact and attachment impl classes to reflect nullable parameters.

--- smartsheet-csharp-sdk/main/Smartsheet/Api/AttachmentVersioningResources.cs
+++ smartsheet-csharp-sdk/main/Smartsheet/Api/AttachmentVersioningResources.cs
@@ -47,7 +47,7 @@ namespace Smartsheet.Api
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        Attachment AttachNewVersion(long sheetId, long attachmentId, string? file, string? fileType);
+        Attachment AttachNewVersion(long sheetId, long attachmentId, string file, string? fileType);
 
         /// <summary>
         /// <para>Deletes all versions of the attachment corresponding to the specified Attachment ID.
