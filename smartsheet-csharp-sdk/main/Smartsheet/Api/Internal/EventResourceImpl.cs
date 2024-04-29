15563897054e66c0925eece77da1ca9f9dc6156b
Tim Hodges <tim.hodges@smartsheet.com>
2024-04-29 15:59:20 -0700

    Making since and stream position nullable for EventResources

--- smartsheet-csharp-sdk/main/Smartsheet/Api/Internal/EventResourceImpl.cs
+++ smartsheet-csharp-sdk/main/Smartsheet/Api/Internal/EventResourceImpl.cs
@@ -63,7 +63,7 @@ namespace Smartsheet.Api.Internal
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        public virtual EventResult ListEvents(object since, string streamPosition, int? maxCount, bool? numericDates)
+        public virtual EventResult ListEvents(object? since, string? streamPosition, int? maxCount, bool? numericDates)
         {
             IDictionary<string, string> parameters = new Dictionary<string, string>();
             if(since != null)
