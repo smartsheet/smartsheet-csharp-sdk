c591ce11e07e9e49a24fe9869158094b29b8b118
Tim Hodges <tim.hodges@smartsheet.com>
2024-04-30 16:20:43 -0700

    Fixing up listing of automation rules to have optional pagination parameters. Also, added a test case for this.

--- mock-api-test-sdk-net80/AutomationRulesTest.cs
+++ mock-api-test-sdk-net80/AutomationRulesTest.cs
@@ -11,7 +11,11 @@ namespace mock_api_test_sdk_net80
         public void ListAutomationRules()
         {
             SmartsheetClient ss = HelperFunctions.SetupClient("List Automation Rules");
-            PaginatedResult<AutomationRule> automationRules = ss.SheetResources.AutomationRuleResources.ListAutomationRules(324, null);
+            PaginatedResult<AutomationRule> automationRules = ss.SheetResources.AutomationRuleResources.ListAutomationRules(324);
+            Assert.AreEqual(2, (long)automationRules.TotalCount);
+
+            PaginationParameters paginationParameters = new PaginationParameters(true, 100, 1);
+            automationRules = ss.SheetResources.AutomationRuleResources.ListAutomationRules(324, paginationParameters);
             Assert.AreEqual(2, (long)automationRules.TotalCount);
         }
 
