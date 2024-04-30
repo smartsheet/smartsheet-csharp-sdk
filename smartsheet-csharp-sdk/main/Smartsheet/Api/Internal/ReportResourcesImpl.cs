fdeab70f36dd12b434471a4c38006052c4430d1d
Tim Hodges <tim.hodges@smartsheet.com>
2024-04-30 14:17:04 -0700

    Updating get and list reports to have optional parameters to make it easier to work with.

--- smartsheet-csharp-sdk/main/Smartsheet/Api/Internal/ReportResourcesImpl.cs
+++ smartsheet-csharp-sdk/main/Smartsheet/Api/Internal/ReportResourcesImpl.cs
@@ -71,7 +71,7 @@ namespace Smartsheet.Api.Internal
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        public virtual Report GetReport(long reportId, IEnumerable<ReportInclusion> includes, int? pageSize, int? page)
+        public virtual Report GetReport(long reportId, IEnumerable<ReportInclusion>? includes, int? pageSize, int? page)
         {
             return GetReport(reportId, includes, pageSize, page, null);
         }
@@ -96,7 +96,7 @@ namespace Smartsheet.Api.Internal
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        public virtual Report GetReport(long reportId, IEnumerable<ReportInclusion> includes, int? pageSize, int? page, int? level)
+        public virtual Report GetReport(long reportId, IEnumerable<ReportInclusion>? includes, int? pageSize, int? page, int? level)
         {
             IDictionary<string, string> parameters = new Dictionary<string, string>();
             if (includes != null)
@@ -138,7 +138,7 @@ namespace Smartsheet.Api.Internal
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        public virtual PaginatedResult<Report> ListReports(PaginationParameters paging, DateTime? modifiedSince)
+        public virtual PaginatedResult<Report> ListReports(PaginationParameters? paging, DateTime? modifiedSince)
         {
             IDictionary<string, string> parameters = new Dictionary<string, string>();
             if (paging != null)
