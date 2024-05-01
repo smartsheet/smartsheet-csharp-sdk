c1864812995d36d5b2043080f1b6cfd7996d6880
Tim Hodges <tim.hodges@smartsheet.com>
2024-05-01 11:55:41 -0700

    Token resources updated to have optional parameters

--- smartsheet-csharp-sdk/main/Smartsheet/Api/TokenResources.cs
+++ smartsheet-csharp-sdk/main/Smartsheet/Api/TokenResources.cs
@@ -76,7 +76,7 @@ namespace Smartsheet.Api
         /// <exception cref="ResourceNotFoundException"> if the resource cannot be found </exception>
         /// <exception cref="ServiceUnavailableException"> if the REST API service is not available (possibly due to rate limiting) </exception>
         /// <exception cref="SmartsheetException"> if there is any other error during the operation </exception>
-        void RevokeAccessToken(bool? deleteAllForApiClient);
+        void RevokeAccessToken(bool? deleteAllForApiClient = null);
     }
 
 }
