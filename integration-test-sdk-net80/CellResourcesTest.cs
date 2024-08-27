61590dfccc71cdb55046df60e7a58f0b0a278e31
John Simon <john.simon@smartsheet.com>
2024-08-27 08:34:09 -0700

    remove extra nulls

--- integration-test-sdk-net80/CellResourcesTest.cs
+++ integration-test-sdk-net80/CellResourcesTest.cs
@@ -13,7 +13,7 @@ namespace integration_test_sdk_net80
             
             long sheetId = CreateSheet(smartsheet);
 
-            PaginatedResult<Column> columnsResult = smartsheet.SheetResources.ColumnResources.ListColumns(sheetId, null, null);
+            PaginatedResult<Column> columnsResult = smartsheet.SheetResources.ColumnResources.ListColumns(sheetId);
             long columnId = columnsResult.Data[0].Id.Value;
 
             Cell[] cellsToAdd = new Cell[] { new Cell.AddCellBuilder(columnId, true).SetValue("hello").SetStrict(false).Build() };
