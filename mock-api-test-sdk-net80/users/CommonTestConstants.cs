31266624137f529ecd31c54e4fd28d9de25245fb
denislavstanchev <denislav.stanchev@smartsheet.com>
2025-11-03 16:31:17 +0200

    Revert "Add user upgrade and downgrade wiremock tests, remove UserResourcesContractTest, refactor folder and file structure for User endpoints tests"
    This reverts commit e6abfb68847fe246cbea6dbfbdfe03f9a7fe85de.

--- mock-api-test-sdk-net80/users/CommonTestConstants.cs
+++
@@ -1,8 +0,0 @@
-namespace mock_api_test_sdk_net80
-{
-    public class CommonTestConstants
-    {
-        public const long TEST_USER_ID = 1234567890L;
-        public const long TEST_PLAN_ID = 1234567890123456L;
-    }
-}
